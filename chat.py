9201eb0817e5c328a28c2e149a39f21a85534156
NaIrW <nairwx@gmail.com>
2023-08-24 21:03:28 +0800

    pq-crystals

--- chat.py
+++ chat.py
@@ -5,7 +5,38 @@ import ctypes
 
 
 lib = ctypes.CDLL('./secp256k1.so')
+
+lib.scalar_multiplication.argtypes = [ctypes.c_char_p, ctypes.c_char_p]
+lib.scalar_multiplication.restype = ctypes.c_int
+
+lib.point_multiplication.argtypes = [ctypes.c_char_p, ctypes.c_char_p, ctypes.c_char_p]
+lib.point_multiplication.restype = ctypes.c_int
+
+lib.point_addition.argtypes = [ctypes.c_char_p, ctypes.c_char_p, ctypes.c_char_p]
+lib.point_addition.restype = ctypes.c_int
+
 lib.init_secp256_lib()
+
+
+pqlib = ctypes.CDLL('./pq-crystals.so')
+
+pqlib.kem_key.argtypes = [ctypes.c_char_p, ctypes.c_char_p]
+pqlib.kem_key.restype = ctypes.c_int
+
+pqlib.kem_enc.argtypes = [ctypes.c_char_p, ctypes.c_char_p, ctypes.c_char_p]
+pqlib.kem_enc.restype = ctypes.c_int
+
+pqlib.kem_dec.argtypes = [ctypes.c_char_p, ctypes.c_char_p, ctypes.c_char_p]
+pqlib.kem_dec.restype = ctypes.c_int
+
+pqlib.sign_key.argtypes = [ctypes.c_char_p, ctypes.c_char_p]
+pqlib.sign_key.restype = ctypes.c_int
+
+pqlib.sign.argtypes = [ctypes.c_char_p, ctypes.c_size_t, ctypes.c_char_p, ctypes.c_size_t, ctypes.c_char_p]
+pqlib.sign.restype = ctypes.c_int
+
+pqlib.verify.argtypes = [ctypes.c_char_p, ctypes.c_size_t, ctypes.c_char_p, ctypes.c_size_t, ctypes.c_char_p]
+pqlib.verify.restype = ctypes.c_int
 
 n = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141
 
@@ -49,6 +80,44 @@ def verifzk(nizk, point):
     R, z = bytes.fromhex(nizk[:130]), int(nizk[130:], 16)
     c = int(Hash(point + R), 16)
     return kG(z) == add(R, kP(c, point))
+
+
+def kem_keypair():
+    pk, sk = b''.zfill(1568), b''.zfill(3168)
+    pqlib.kem_key(pk, sk)
+    pk, sk = bytes(bytearray(pk)), bytes(bytearray(sk))
+    return pk, sk
+
+
+def enc(pk):
+    ct = b''.zfill(1568)
+    ss = b''.zfill(32)
+    pqlib.kem_enc(ct, ss, pk)
+    ss, ct = bytes(bytearray(ss)), bytes(bytearray(ct))
+    return ss, ct
+
+
+def dec(ct, sk):
+    ano = b''.zfill(32)
+    pqlib.kem_dec(ano, ct, sk)
+    return bytes(bytearray(ano))
+
+
+def sign_keypair():
+    pk, sk = b''.zfill(2592), b''.zfill(4864)
+    pqlib.sign_key(pk, sk)
+    pk, sk = bytes(bytearray(pk)), bytes(bytearray(sk))
+    return pk, sk
+
+
+def sign(m, sk):
+    sig = b''.zfill(4595)
+    pqlib.sign(sig, 4595, m, len(m), sk)
+    return bytes(bytearray(sig))
+
+
+def verify(sig, m, pk):
+    return pqlib.verify(sig, 4595, m, len(m), pk) == 0
 
 
 def handleChat(item_dict):
